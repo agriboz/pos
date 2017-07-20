2613056f6dc941d971a43d18cb1023abda34a623
Can Ağrıboz <agriboz@gmail.com>
2017-07-20 14:56:44 +0300

    .

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -10,30 +10,17 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    desiredCapabilities: {
-      browserName: process.env.BROWSER || 'chrome',
-      chromeOptions: {
-        args: [
-          '--headless',
-          '--no-sandbox'
-        ],
-        prefs: {
-          'download.default_directory': __dirname + '/tmp/test/download',
-          'download.prompt_for_download': false
-        }
-      }
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true,
     },
     angularCli: {
-      environment: 'dev'
+      environment: 'dev',
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -41,6 +28,20 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    desiredCapabilities: {
+      browserName: process.env.BROWSER || 'chrome',
+      chromeOptions: {
+        args: [
+          // '--enable-logging',
+          '--headless',
+          '--no-sandbox',
+        ],
+        prefs: {
+          'download.default_directory': __dirname + '/tmp/test/download',
+          'download.prompt_for_download': false,
+        },
+      },
+    },
   });
 };
